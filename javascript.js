514d9ed942a6da65b61fb2538afc45e09227aabc
OlenaKaminska <a.kaminskaya12@gmail.com>
2022-07-15 10:47:22 +0300

    Showing the current temperature

--- javascript.js
+++ javascript.js
@@ -1,2 +1,15 @@
+function showTemperature(response) {
+  let temperature = document.querySelector("#temperature");
+  temperature.innerHTML = Math.round(response.data.main.temp);
+  let city = document.querySelector("#city");
+  city.innerHTML = response.data.name;
+  let description = document.querySelector("#description");
+  description.innerHTML = response.data.weather[0].description;
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = response.data.main.humidity;
+  let wind = document.querySelector("#wind");
+  wind.innerHTML = Math.round(response.data.wind.speed);
+}
 let apiKey = "1b4add62678c7e2572d5cd6a74fa9dfe";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(showTemperature);
