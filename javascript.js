e3215a5326d5c3f2b42018e05e220da4d48e4c7f
OlenaKaminska <a.kaminskaya12@gmail.com>
2022-07-18 17:21:32 +0300

    Added forecast grid by JS

--- javascript.js
+++ javascript.js
@@ -20,6 +20,32 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
+
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/02d@2x.png"
+          alt=""
+          width="54"
+        />
+        <div class="weather-forecast-temperature">
+          <span class="weather-forecast-temperature-max">18°</span>
+          <span class="weather-forecast-temperature-min"> 12°</span>
+        </div>
+      </div>
+    `;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
 function showTemperature(response) {
   celsiusTemperature = response.data.main.temp;
   let temperature = document.querySelector("#temperature");
@@ -81,3 +107,4 @@ let celsiusLink = document.querySelector("#Celsius-link");
 celsiusLink.addEventListener("click", showCelsiusTemperature);
 
 search("New York");
+displayForecast();
